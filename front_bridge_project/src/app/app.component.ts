4964f77d784d9aac7e40cbf23847219c8269e921
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-01-13 16:47:25 +0100

    refactor: app

--- front_bridge_project/src/app/app.component.ts
+++ front_bridge_project/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SvgShapeComponent } from './svg-shape/svg-shape.component';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SvgShapeComponent,
+    LeafletMapComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'front_bridge_project';
+  title = "Hydrometrica";
+  purpose = "Visualisez et analysez les données hydrométriques de notre région!";
+  region = "Alpes-Maritimes (06)";
 }
