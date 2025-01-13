f4c352a361440f9efc4268138ba780e74a491ea9
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-01-13 16:45:34 +0100

    feat: environment config

---
+++ front_bridge_project/environment.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'http://127.0.0.1:8000/api';
+
+export const environment = {
+  baseUrl,
+  bridgesApi: `${baseUrl}/bridges`,
+  hubeauApi: "https://hubeau.eaufrance.fr/api/v1/hydrometrie"
+};
